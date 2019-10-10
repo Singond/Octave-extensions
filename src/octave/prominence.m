4a713bbb1b74c2689aa7c1d5b4a8459ae7e86bac
Singon <singond@seznam.cz>
2019-10-11 01:22:03 +0200

    Add test

--- src/octave/prominence.m
+++ src/octave/prominence.m
@@ -53,3 +53,6 @@ endfunction
 %!assert(isol([1 4 8 7 2 1 4 2 5 9 1],  3),  [1 10]);
 %!assert(isol([1 4 8 7 2 1 4 2 5 9 1],  7),  [4 9]);
 %!assert(isol([1 4 8 7 2 1 4 2 5 9 1],  10), [1 11]);
+%!
+%!# Behaviour at right edge
+%!assert(isol([1 4 8 7 2 1 4 2 5 9 10], 11), [1 11]);
