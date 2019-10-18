6268fa8cd84c1b126f75ad7b62a8828ddddc60ea
Jan Slany <jan.slany@ais-servis.cz>
2019-10-18 18:59:25 +0200

    Add function for peak width calculation

---
+++ src/octave/peakwidth.m
@@ -0,0 +1,34 @@
+function [w, ext] = peakwidth(x, y, p, h)
+	## TODO: Enable omitting 'x' altogether
+	if (isempty(x))
+		x = 1:length(y);
+	endif
+	if (!size_equal(x, y))
+		error("x and y must have the same size");
+	endif
+	if (p < min(x) || p > max(x))
+		error("Position out of bounds");
+	endif
+
+	L = find(y < h);
+	Lleft = L(L < p);
+	if (!isempty(Lleft))
+		low = max(Lleft);
+		high = low + 1;
+		left = interp1(y([low high]), x([low high]), h);
+	else
+		left = 1;
+	endif
+	Lright = L(L > p);
+	if (!isempty(Lright))
+		low = min(Lright);
+		high = low - 1;
+		right = interp1(y([high low]), x([high low]), h);
+	else
+		right = length(x);
+	endif
+	w = right - left;
+	if (nargin > 1)
+		ext = [left right];
+	endif;
+endfunction
