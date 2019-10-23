c1153c6af8d2032ccdcc5dd9341a7ff5706fc3cf
Jan Singon Slany <slanyj@centrum.cz>
2019-10-23 23:12:06 +0200

    Make peakwidth accept vector arguments

--- src/octave/peakwidth.m
+++ src/octave/peakwidth.m
@@ -6,10 +6,26 @@ function [w, ext] = peakwidth(y, p, h)
 	if (!size_equal(x, y))
 		error("x and y must have the same size");
 	endif
+	if (!size_equal(p, h))
+		error("p and h must have the same size");
+	endif
 	if (p < min(x) || p > max(x))
 		error("Position out of bounds");
 	endif
 
+	if (isscalar(p))
+		[w, ext] = peakwidth_point(y, x, p, h);
+	else
+		p = p(:);
+		h = h(:);
+		[w, ext] = arrayfun(@(_p, _h) peakwidth_point(y, x, _p, _h), p, h, ...
+				"UniformOutput", false);
+		w = cell2mat(w);
+		ext = cell2mat(ext);
+	endif
+endfunction
+
+function [w, ext] = peakwidth_point(y, x, p, h)
 	L = find(y < h);
 	Lleft = L(L < p);
 	if (!isempty(Lleft))
