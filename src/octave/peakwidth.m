f07cbfa7ad24320a10ad3992827282bed51000cb
Jan Singon Slany <singond@seznam.cz>
2019-10-30 23:02:39 +0100

    Add license

--- src/octave/peakwidth.m
+++ src/octave/peakwidth.m
@@ -1,3 +1,19 @@
+## Copyright (C) 2019 Jan Slany
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING.  If not, see
+## <https://www.gnu.org/licenses/>.
+
 ## -*- texinfo -*-
 ## @deftypefn  {Function file} {w =} peakwidth(data, loc, h)
 ## @deftypefnx {Function file} {[w, ext] =} peakwidth(data, loc, h)
@@ -13,6 +29,10 @@
 ## The optional return value @var{ext} is a two column matrix containing the
 ## crossing points for each measured peak.
 ## @end deftypefn
+
+## Author: Jan "Singon" Slany <singond@seznam.cz>
+## Created: October 2019
+## Keywords: signal processing, peak finding
 function [w, ext] = peakwidth(y, p, h)
 	x = [1:length(y)]';
 	if (!iscolumn(y))
