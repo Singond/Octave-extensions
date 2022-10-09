a41f1bf01c4976ef6a1adfecbc337cefef799886
Jan Singon Slany <singond@seznam.cz>
2022-10-09 10:42:48 +0200

    Add license and copyright to function header

--- src/octave/animate.m
+++ src/octave/animate.m
@@ -1,3 +1,19 @@
+## Copyright (C) 2019 Jan Slany
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program; see the file COPYING.  If not, see
+## <https://www.gnu.org/licenses/>.
+
 ## -*- texinfo -*-
 ## @deftypefn  {} {} animate (@var{img})
 ## @deftypefnx {} {} animate (@var{img}, @var{framelength})
@@ -17,6 +33,10 @@
 ## The frames are displayed using @code{imshow}.
 ## Any unmatched arguments are passed to this function.
 ## @end deftypefn
+
+## Author: Jan "Singon" Slany <singond@seznam.cz>
+## Created: October 2019
+## Keywords: signal processing, peak finding
 function animate(img, framelength = 1, varargin)
 	nframes = size(img, 3);
 
