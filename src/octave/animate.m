4e63ce3a1ee301940287c825a5611cfc794fd6f3
Jan Singon Slany <singond@seznam.cz>
2025-08-30 20:05:48 +0200

    Fix year in copyright statement

--- src/octave/animate.m
+++ src/octave/animate.m
@@ -1,4 +1,4 @@
-## Copyright (C) 2019 Jan Slany
+## Copyright (C) 2022 Jan Slany
 ##
 ## This program is free software: you can redistribute it and/or modify
 ## it under the terms of the GNU General Public License as published by
